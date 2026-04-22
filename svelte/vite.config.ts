81a0a5d2c8465e93e25552a2e72117be2f634fe3
Fofow <1760855795@qq.com>
2026-04-23 03:41:52 +0800

    svelte加入工作区

--- svelte/vite.config.ts
+++ svelte/vite.config.ts
@@ -9,9 +9,16 @@ export default defineConfig({
 		sveltekit(),
 		paraglideVitePlugin({ project: './project.inlang', outdir: './src/lib/paraglide' })
 	],
-		server: {
+	server: {
 		host: true, // 或者 '0.0.0.0'
 		strictPort: false, // 如果端口被占用，自动尝试下一个可用端口
-		cors:true
-	},
+		cors: true,
+		fs: {
+			allow: [
+				'C:/Users/Administrator/Desktop/GameStorm',
+				// 添加 pnpm 的虚拟存储目录
+				'C:/Users/Administrator/Desktop/GameStorm/node_modules/.pnpm'
+			]
+		}
+	}
 });
